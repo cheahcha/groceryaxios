20506ce1d039d90610d935c54deec63e4ac6a96d
cheahcha <61797648+cheahcha@users.noreply.github.com>
2020-03-26 02:26:39 +0800

    Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAtE6XoGewdmYMXI-89mygK3uyUfNZyzjk',
-  authDomain: 'bt3103-5a943.firebaseapp.com',
-  databaseURL: 'https://bt3103-5a943.firebaseio.com',
-  projectId: 'bt3103-5a943',
-  storageBucket: 'bt3103-5a943.appspot.com',
-  messagingSenderId: '421096000425',
-  appId: '1:421096000425:web:4b36e92faa8f393978d2b8',
-  measurementId: 'G-FLB20M676C'
+var firebaseConfig = {
+  apiKey: 'AIzaSyB5B5CxnpV900fU0E-r8YO5jHqTvyE6L-Q',
+  authDomain: 'groceryaxios-78d4b.firebaseapp.com',
+  databaseURL: 'https://groceryaxios-78d4b.firebaseio.com',
+  projectId: 'groceryaxios-78d4b',
+  storageBucket: 'groceryaxios-78d4b.appspot.com',
+  messagingSenderId: '590950854977',
+  appId: '1:590950854977:web:bdb2c725eec3d48b1ab1c0',
+  measurementId: 'G-RLE34PG8XP'
 };
 
 firebase.initializeApp(firebaseConfig);
